b1424fa28464fed3c7892dd6a8bb5b3c4fdf4789
joseph.berkovitz <joseph.berkovitz@db935c10-044b-0410-8d30-b3df90821228>
2008-04-28 18:08:22 +0000

    Created tagged cut of flexcover SDK which tests as compatible with flexcover 0.10

---
+++ tags/flexcover-0.10-compatible/flex3.0.x/modules/asc/src/java/macromedia/asc/embedding/avmplus/DebugInfo.java
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  ADOBE SYSTEMS INCORPORATED
+//  Copyright 2004-2007 Adobe Systems Incorporated
+//  All Rights Reserved.
+//
+//  NOTICE: Adobe permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package macromedia.asc.embedding.avmplus;
+
+/**
+ * DebugInfo
+ *
+ * @author Gary Grossman
+ */
+public class DebugInfo
+{
+    int debug_linenum = -1;
+    boolean debug_linenum_dirty = false;
+    String debug_file;
+    boolean suppress_debug_method = false;
+    boolean debug_file_dirty = false;
+    String debug_function;
+    boolean debug_function_dirty = false;
+    
+    boolean branch_conditional_dirty = false;
+    boolean branch_target_dirty = false;
+}
+
